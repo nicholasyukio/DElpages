d9bd5ce5b00b19195c14e72e113b9247a6398db7
Nicholas Yukio <nicholasyukio@gmail.com>
2024-04-02 21:07:30 -0300

    New price

--- src/pages/oferta_breve.js
+++ src/pages/oferta_breve.js
@@ -13,7 +13,7 @@ const OfertaBreve = () => {
         });
 
         // Redirect the user after pushing the data to GTM if needed
-        window.location.href = 'https://pague.lia.com.br/dominio-eletrico/oferta?offer_id=2fd10c29-92d0-40a3-b404-76cdc09d59cd'; // Redirect to the form anchor
+        window.location.href = 'https://pague.lia.com.br/dominio-eletrico/oferta?offer_id=32655ab6-c847-4db0-ab62-c49f342487e4'; // Redirect to the form anchor
     };
 
     const handleClickForPix = () => {
@@ -24,22 +24,21 @@ const OfertaBreve = () => {
         });
 
         // Redirect the user after pushing the data to GTM if needed
-        window.location.href = 'https://pague.lia.com.br/dominio-eletrico/oferta?offer_id=e4f62bd4-4dd7-444e-9efb-c17331df763a'; // Redirect to the form anchor
+        window.location.href = 'https://pague.lia.com.br/dominio-eletrico/oferta?offer_id=d997bb7c-4e4b-4472-95ce-50ad197a5dfa'; // Redirect to the form anchor
     };
 
 	return (
     <>
     <section id="form" className="section">
     <div className="offer-container">
-        <h2 align="center">Oferta especial de final de março/2024:</h2>
-        <h2 align="center">de: <OriginalPrice />,<br /> por: <OfferPrice /></h2>
+        <h2 align="center">Preço para você assinar a plataforma do curso:</h2>
+        <h2 align="center"><OriginalPrice /></h2>
         <h2 align="left">Formas de pagamento:</h2>
         <table>
             <tr>
                 <td>
                     <h4 align="left">Parcelado no cartão de crédito <br/>(juros de 2% a.m.):</h4>
-                    <p>de: <OriginalPriceParcelado12x/></p>
-                    <p>por: <OfferPriceParcelado12x/></p>
+                    <p><OriginalPriceParcelado12x/></p>
                 </td>
                 <td>
                     <button className="btn-pagamento" onClick={handleClickForCreditCard}>Pagar com cartão de crédito</button>
@@ -48,8 +47,7 @@ const OfertaBreve = () => {
             <tr>
                 <td>
                     <h4 align="left">À vista no Pix ou boleto <br/>(5% de desconto):</h4>
-                    <p>de: <OriginalPricePix/></p>
-                    <p>por: <OfferPricePix/></p>
+                    <p><OriginalPricePix/></p>
                 </td>
                 <td>
                     <button className="btn-pagamento" onClick={handleClickForPix}>Pagar com Pix/boleto</button>
