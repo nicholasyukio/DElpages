c06fb2867f0e9672186b0c626fd94b930ce68652
Nicholas Yukio <nicholasyukio@gmail.com>
2024-04-12 11:28:32 -0300

    New attempt of solving trailing slash issue

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Navigate,
-  useLocation
+  Route
 } from "react-router-dom";
 import { Home , Espera, Direto } from "./pages/index";
 import PoliticaDePrivacidade from "./pages/politicadeprivacidade";
@@ -13,21 +11,9 @@ import TermosDeUso from "./pages/termosdeuso";
 import Oferta from "./pages/oferta";
 import Login from "./pages/login";
 
-const RemoveTrailingSlash = ({...rest}) => {
-  const location = useLocation()
-  // If the last character of the url is '/'
-  if (location.pathname.match('/.*/$')) {
-      return <Navigate replace {...rest} to={{
-          pathname: location.pathname.replace(/\/+$/, ""),
-          search: location.search
-      }}/>
-  } else return null
-}
-
 export default function App() {
   return (
     <Router>
-      <RemoveTrailingSlash />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/espera-dominio-eletrico" element={<Espera />} />
