a14535571d827231c831562971997fa88f611c6d
Nicholas Yukio <nicholasyukio@gmail.com>
2024-04-12 11:23:03 -0300

    RemoveTrailingSlash again

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, Espera, Direto } from "./pages/index";
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation
+} from "react-router-dom";
+import { Home , Espera, Direto } from "./pages/index";
 import PoliticaDePrivacidade from "./pages/politicadeprivacidade";
 import TermosDeUso from "./pages/termosdeuso";
 import Oferta from "./pages/oferta";
 import Login from "./pages/login";
 
-// Custom route component to handle redirection of routes with trailing slashes
-const CustomRoute = ({ path, element }) => {
-  const hasTrailingSlash = path.endsWith("/");
-  if (hasTrailingSlash && path !== "/") {
-    const newPath = path.slice(0, -1);
-    return <Route path={newPath} element={element} />;
-  }
-  return <Route path={path} element={element} />;
-};
+const RemoveTrailingSlash = ({...rest}) => {
+  const location = useLocation()
+  // If the last character of the url is '/'
+  if (location.pathname.match('/.*/$')) {
+      return <Navigate replace {...rest} to={{
+          pathname: location.pathname.replace(/\/+$/, ""),
+          search: location.search
+      }}/>
+  } else return null
+}
 
 export default function App() {
   return (
     <Router>
+      <RemoveTrailingSlash />
       <Routes>
-        {/* Use CustomRoute instead of Route */}
-        <CustomRoute path="/" element={<Home />} />
-        <CustomRoute path="/espera-dominio-eletrico" element={<Espera />} />
-        <CustomRoute path="/direto-dominio-eletrico" element={<Direto />} />
-        <CustomRoute
-          path="/politicadeprivacidade"
-          element={<PoliticaDePrivacidade />}
-        />
-        <CustomRoute path="/termosdeuso" element={<TermosDeUso />} />
-        <CustomRoute path="/oferta" element={<Oferta />} />
-        <CustomRoute path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/espera-dominio-eletrico" element={<Espera />} />
+        <Route path="/direto-dominio-eletrico" element={<Direto />} />
+        <Route path="/politicadeprivacidade" element={<PoliticaDePrivacidade />} />
+        <Route path="/termosdeuso" element={<TermosDeUso />} />
+        <Route path="/oferta" element={<Oferta />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
-  );
+);
 }
